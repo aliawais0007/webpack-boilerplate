8fe80de5754760ccd5d510329c96ddcc39c951d9
aliawais0007 <ali.awais5044@gmail.com>
2021-08-31 00:44:13 +0400

    first commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import { home } from "./home";
+import css from "./index.css";
+let a = 0;
+for (a; a < 10; a++) {
+    console.log(a)
+}
+
+console.log("final: " + a + "import: " + home)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+module.exports = {
+    // Where to start bundling
+    entry: {
+        app: "./app/index.js"
+    },
+    // Where to output
+    output: {
+        // Output to the same directory
+        path: __dirname,
+        // Capture name from the entry using a pattern
+        filename: "bundle.js",
+    },
+    // How to resolve encountered imports
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    // What extra processing to perform
+    plugins: [
+        new webpack.DefinePlugin({}),
+    ],
+    // Adjust module resolution algorithm
+    resolve: {
+        alias: {},
+    },
+};
